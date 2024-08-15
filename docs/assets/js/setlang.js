1e321161d4de599b3a86e5b365355ba6f76cdc38
Blackture <gamingblackture@gmail.com>
2024-08-14 17:06:46 -0700

    Delete Webpage

--- docs/assets/js/setlang.js
+++
@@ -1,8 +0,0 @@
-function SetLanguage() {
-    if (window.navigator.language == "de-DE" || window.navigator.language == "de")  {
-        window.open("de/index.html", "_self");
-    }
-    else {
-        window.open("en/index.html", "_self");
-    }
-}
